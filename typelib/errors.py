9a2b4ca693fd6256889d2a9253068a3fae43f126
Sriram Panyam <sri.panyam@gmail.com>
2016-06-18 19:04:20 -0700

    Cleanups

--- typelib/errors.py
+++ typelib/errors.py
@@ -1,25 +1,25 @@
 
-class OneringException(Exception):
+class TLException(Exception):
     def __init__(self, msg):
         Exception.__init__(self, msg)
 
-class FieldNotFoundException(OneringException):
+class FieldNotFoundException(TLException):
     def __init__(self, field_name, parent_type):
         Exception.__init__(self, "Field '%s' not found in record: %s" % (field_name, parent_type.fqn))
 
-class DuplicateTypeException(OneringException):
+class DuplicateTypeException(TLException):
     def __init__(self, type_name):
         Exception.__init__(self, "Type '%s' is already defined" % type_name)
 
-class DuplicateFieldException(OneringException):
+class DuplicateFieldException(TLException):
     def __init__(self, field_name, parent_type):
         Exception.__init__(self, "Duplicate Field '%s' encountered in record: %s" % (field_name, parent_type.fqn))
 
-class TransformerException(OneringException):
+class TransformerException(TLException):
     def __init__(self, msg):
         Exception.__init__(self, msg)
  
-class TypeNotFoundException(OneringException):
+class TypeNotFoundException(TLException):
     def __init__(self, fqn):
         Exception.__init__(self, "Type '%s' not found.  Are class and/or jar paths set correctly?" % fqn)
         self.missing_type = fqn
