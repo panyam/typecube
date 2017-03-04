dcc42ea2974c12dc1fa05c7815a4f7f375eb8515
Sriram Panyam <spanyam@linkedin.com>
2017-03-03 23:29:13 -0800

    Enabling values within enums

--- typelib/enums.py
+++ typelib/enums.py
@@ -4,12 +4,13 @@ from typelib.annotations import Annotatable
 import utils
 
 class EnumSymbol(Annotatable):
-    def __init__(self, name, annotations = None, docs = ""):
+    def __init__(self, name, value = None, annotations = None, docs = ""):
         Annotatable.__init__(self, annotations, docs)
         self.name = name
+        self.value = value
 
-def EnumType(symbols = None, annotations = None, docs = None):
-    out = core.Type(None, "enum", type_params = None, type_args = None, annotations = annotations, docs = docs)
+def EnumType(symbols = None, type_args = None, annotations = None, docs = None):
+    out = core.Type(None, "enum", type_params = None, type_args = type_args, annotations = annotations, docs = docs)
     out.type_data = symbols
     return out
 
