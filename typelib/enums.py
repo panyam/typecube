846666ee440fe228be947a2bff482b88e78d5202
Sriram Panyam <spanyam@linkedin.com>
2017-04-15 11:03:42 -0700

    Simplifying enums

--- typelib/enums.py
+++ typelib/enums.py
@@ -1,10 +1,10 @@
-
+import ipdb
 import core
 from typelib.annotations import Annotatable
 import utils
 
 def EnumSymbol(name, parent, value = None, annotations = None, docs = ""):
-    out = core.Type(name, parent, "literal", annotations = symbol.annotations, docs = symbol.docs)
+    out = core.Type(name, parent, "literal", type_params = None, type_args = None, annotations = annotations, docs = docs)
     out.value = value
     return out
 
@@ -12,6 +12,6 @@ def EnumType(name, parent, symbols = None, type_args = None, annotations = None,
     out = core.Type(name, parent, "enum", type_params = None, type_args = type_args, annotations = annotations, docs = docs)
     symbols = symbols or []
     for symbol in symbols:
-        out.add_entity(symbol)
+        out.add(symbol)
     return out
 
