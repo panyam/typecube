b256e9744450d43ce8883205d20184f17a047285
Sriram Panyam <spanyam@linkedin.com>
2017-04-19 17:21:05 -0700

    Better arg name

--- typelib/unifier.py
+++ typelib/unifier.py
@@ -3,34 +3,35 @@ import ipdb
 from core import AnyType
 from itertools import izip
 
-def can_substitute(type1, type2):
+def can_substitute(peg_type, hole_type):
     """
-    Returns True if type2 can be substituted for type1 when passed as a function.
+    Returns True if peg_type can fit into a hole_type (ie be substituted for when called as an argument to a function).
     This checks the types recursively.
     """
-    if type1 == type2 or type1 == AnyType:
+    if not peg_type or not hole_type: ipdb.set_trace()
+    if peg_type == hole_type or hole_type == AnyType:
         return True
 
-    if type1.constructor != type2.constructor:
+    if peg_type.constructor != hole_type.constructor:
         return False
 
-    if type1.is_sum_type != type2.is_sum_type:
+    if peg_type.is_sum_type != hole_type.is_sum_type:
         return False
 
-    if type1.name != type2.name:
+    if peg_type.name != hole_type.name:
         return False
 
-    if type1.name:  # if a name was provided then check for parents
-        if type1.parent != type2.parent:
+    if peg_type.name:  # if a name was provided then check for parents
+        if peg_type.parent != hole_type.parent:
             return False
 
-        if type1.parent.fqn != type2.parent.fqn:
+        if peg_type.parent.fqn != hole_type.parent.fqn:
             return False
 
-    if type1.argcount != type2.argcount:
+    if peg_type.argcount != hole_type.argcount:
         return False
 
-    for arg1,arg2 in izip(type1.args, type2.args):
+    for arg1,arg2 in izip(peg_type.args, hole_type.args):
         if not can_substitute(arg1.typeref.final_entity, arg2.typeref.final_entity):
             return False
 
