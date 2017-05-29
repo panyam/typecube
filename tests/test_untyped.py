ad82db3c322e45a85f1e2f484470e6233bb0826c
Sriram Panyam <spanyam@linkedin.com>
2017-05-29 13:40:18 -0700

    Alpha conversion performed on the fly during substitutions

--- tests/test_untyped.py
+++ tests/test_untyped.py
@@ -33,16 +33,46 @@ def test_abstraction():
 
 def test_reduce():
     v = Var("x")
-    assert v == v.reduce()
+    assert v,True == v.reduce()
 
     v = Abs("x", "y")
-    assert v == v.reduce()
+    assert v, False == v.reduce()
 
     v = App("x", "y")
-    assert v == v.reduce()
+    assert v,False == v.reduce()
 
     v = App(Abs("x", "x"), "x")
-    assert v.reduce().name == "x"
+    result,success = v.reduce()
+    assert success and result.name == "x"
+
+    v = App(Abs("x", "x"), "y")
+    result,success = v.reduce()
+    assert success and result.name == "y"
+
+    # This should result in an alpha renaming of the innner "f" since the f that is passed as the
+    # substitution is actually bound to the "n", but doing so without alpha renaming the f
+    # will result in the identity function
+    t,_ = Abs(["n", "f"], "n").apply("f")
+    # ipdb.set_trace()
+    f, success = t.reduce()
+
+    t,_ = church_lit(1).apply(pred)
+    # ipdb.set_trace()
+    f, success = t.reduce()
+
+def test_equiv():
+    assert equiv(Var("x"), Var("y"))
+    assert equiv(Abs("x", "x"), Abs("y", "y"))
+    assert not equiv(Abs("x", "x"), Abs("y", "z"))
+    assert equiv(Abs("x", App("y", "z")), Abs("a", App("b", "c")))
+    assert equiv(Abs("x", App("x", "z")), Abs("a", App("a", "b")))
+    assert not equiv(Abs("x", App("y", "z")), Abs("a", App("b", "a")))
+    assert not equiv(Abs("x", App("y", "z")), Abs("a", App("a", "b")))
+    assert equiv(Abs("x", Abs("y", App("x", "y"))), Abs("y", Abs("x", App("y", "x"))))
+    assert equiv(Abs("x", Abs("y", App("x", "y"))), Abs("y", Abs("z", App("y", "z"))))
+
+    # This requires us to work around name bindings too!
+    # assert equiv(Abs("x", Abs("x", App("x", "z"))), Abs("a", Abs("b", App("b", "c"))))
 
 def test_application():
     print "\\x: x y = ", str(Abs("x", Var("x")).substitute("x", "y"))
@@ -56,47 +86,103 @@ def test_booleans():
     v = Var("v")
     w = Var("w")
     out = App(test, true, v, w)
-    f = out.reduce()
+    f,success = out.reduce()
     assert f == v
 
 def test_and():
-    f = App(f_and, true, true).reduce()
+    t = App(f_and, true, true)
+    f,success = t.reduce()
     assert equiv(f, true)
 
-    f = App(f_and, true, false).reduce()
+    t = App(f_and, true, false)
+    f,success = t.reduce()
     assert equiv(f, false)
 
-    f = App(f_and, false, true).reduce()
+    t = App(f_and, false, true)
+    f,success = t.reduce()
     assert equiv(f, false)
 
-    f = App(f_and, false, false).reduce()
+    t = App(f_and, false, false)
+    f,success = t.reduce()
     assert equiv(f, false)
 
 def test_or():
-    f = App(f_or, true, true).reduce()
+    t = App(f_or, true, true)
+    f,success = t.reduce()
     assert equiv(f, true)
 
-    f = App(f_or, true, false).reduce()
+    t = App(f_or, true, false)
+    f,success = t.reduce()
     assert equiv(f, true)
 
-    f = App(f_or, false, true).reduce()
+    t = App(f_or, false, true)
+    f,success = t.reduce()
     assert equiv(f, true)
 
-    f = App(f_or, false, false).reduce()
+    t = App(f_or, false, false)
+    f,success = t.reduce()
     assert equiv(f, false)
 
 def test_if():
     a = Var("a")
     b = Var("b")
-    f = App(f_if, true, a, b).reduce()
+    t = App(f_if, true, a, b)
+    f,success = t.reduce()
     assert f.name == "a"
 
-    f = App(f_if, false, a, b).reduce()
+    t = App(f_if, false, a, b)
+    f,success = t.reduce()
     assert f.name == "b"
 
 def test_pair():
     a = Var("a")
     b = Var("b")
-    p = App(pair, a, b).reduce()
-    assert App(pair_first, p).reduce().name == a.name
-    assert App(pair_second, p).reduce().name == b.name
+    p,success = App(pair, a, b).reduce()
+    pfirst,success = App(pair_first, p).reduce()
+    psecond,success = App(pair_second, p).reduce()
+    assert pfirst.name == a.name
+    assert psecond.name == b.name
+
+def test_church_numerals_succ():
+    one = church_lit(1)
+    two = church_lit(2)
+    sone = App(succ, zero)
+    f,success = sone.reduce()
+    assert equiv(one, f)
+
+    stwo = App(succ, App(succ, zero))
+    f,success = stwo.reduce()
+    assert equiv(two, f)
+
+def test_church_numerals_pred():
+    one = church_lit(1)
+    pone = App(pred, one)
+    f,success = pone.reduce()
+    assert equiv(zero, f)
+
+    two = church_lit(2)
+    pone = App(pred, App(pred, one))
+    f,success = pone.reduce()
+    assert equiv(zero, f)
+
+def test_church_numerals_add():
+    five = church_lit(5)
+    three = church_lit(3)
+    eight = church_lit(8)
+    result = App(plus, five, three)
+    f,success = result.reduce()
+    assert equiv(eight, f)
+
+def test_church_numerals_minus():
+    eight = church_lit(8)
+    five = church_lit(5)
+    result = App(minus, eight, five)
+    f,success = result.reduce()
+    assert equiv(church_lit(3), f)
+
+def test_church_numerals_mult():
+    five = church_lit(6)
+    three = church_lit(9)
+    result = App(mult, five, three)
+    f,success = result.reduce()
+    assert equiv(church_lit(54), f)
--- typelib/untyped.py
+++ typelib/untyped.py
@@ -1,5 +1,6 @@
 import ipdb
 from itertools import izip
+from collections import defaultdict
 
 def equiv(term1, term2, mapping12 = None, mapping21 = None):
     """ Checks if two terms are equivalent. """
@@ -18,8 +19,9 @@ def equiv(term1, term2, mapping12 = None, mapping21 = None):
         else:
             return False
     elif type(term1) is Abs:
-        return equiv(term1.term, term2.term, mapping12, mapping21) and \
-                equiv(Var(term1.bound_varname), Var(term2.bound_varname), mapping12, mapping21) 
+        if not equiv(Var(term1.varname), Var(term2.varname), mapping12, mapping21):
+            return False
+        return equiv(term1.term, term2.term, mapping12, mapping21)
     else:
         return len(term1.terms) == len(term2.terms) and \
                 all(equiv(t1, t2, mapping12, mapping21) for t1,t2 in izip(term1.terms, term2.terms))
@@ -35,30 +37,35 @@ def normalize_term(term):
 class Term(object):
     """ Top level terms in untyped lambda calculus. """
     def __init__(self):
-        pass
+        self._free_variables = None
+
+    @property
+    def free_variables(self):
+        if self._free_variables is None:
+            self._free_variables = self.eval_free_variables()
+        return self._free_variables
+
+    def eval_free_variables(self):
+        return set()
 
     def substitute(self, name, term):
         """ Substitutes a particular variable by a term in this term. Returns the term after the substitution is done."""
         return None
 
-    @property
-    def free_variables(self):
-        return {}
-
     def reduce_once(self):
         """ Implement this, and return the reduced term which could be this term itself if no reduction was possible. """
-        return self
+        return self, False
 
     def reduce(self):
-        curr = self
-        next = self.reduce_once()
-        while curr != next:
-            curr = next
-            next = next.reduce_once()
-        return next
+        tmp = None
+        curr, reduced = self.reduce_once()
+        success = reduced
+        while reduced:
+            tmp = curr
+            curr, reduced = curr.reduce_once()
+            success = success or reduced
+        return curr,success
 
-
-       
 class Var(Term):
     def __init__(self, name):
         Term.__init__(self)
@@ -66,48 +73,79 @@ class Var(Term):
 
     def substitute(self, name, term):
         """ Substitutes a particular variable by a term in this term. """
-        return normalize_term(term) if self.name == name else self
+        if name == self.name:
+            return normalize_term(term), True
+        return self, True
 
-    def __repr__(self): return "Var(%x): %s" % (id(self), str(self))
+    def __repr__(self): return "<Var(%x): %s>" % (id(self), str(self))
     def __str__(self):
         return self.name
 
-    @property
-    def free_variables(self):
+    def eval_free_variables(self):
         return {self.name}
 
 class Abs(Term):
-    def __init__(self, bound_varname, term):
+    def __init__(self, varnames, term):
         Term.__init__(self)
-        self.bound_varname = bound_varname
-        self.term = normalize_term(term)
+        if type(varnames) in (str, unicode):
+            varnames = [varnames]
+        N = len(varnames)
+        self.varname = varnames[0]
+        out = normalize_term(term)
+        for i in xrange(N - 1, 0, -1):
+            out = Abs(varnames[i], out)
+        self.term = out
 
-    def __repr__(self): return "Abs(%x): %s" % (id(self), str(self))
+    def __repr__(self): return "<Abs(%x): %s>" % (id(self), str(self))
 
     def __str__(self):
-        if type(self.term) is Abs:
-            return "\\%s . (%s)" % (self.bound_varname, str(self.term))
+        names = "\\" + self.varname
+        # return "%s : (%s)" % (names, str(self.term))
+        if type(self.term) in (Abs, Var):
+            return "%s : %s" % (names, str(self.term))
         else:
-            return "\\%s . %s" % (self.bound_varname, str(self.term))
+            return "%s : (%s)" % (names, str(self.term))
 
-    @property
-    def free_variables(self):
+    def eval_free_variables(self):
         out = self.term.free_variables
-        if self.bound_varname in out:
-            out.remove(self.bound_varname)
+        if self.varname in out:
+            out.remove(self.varname)
         return out
 
     def apply(self, term):
-        return self.term.substitute(self.bound_varname, term)
+        out,success = self.term.substitute(self.varname, term)
+        out = out if success else self
+        return out, success
 
-    def substitute(self, name, term):
+    def reduce_once(self):
+        term, reduced = self.term.reduce()
+        out = self if not reduced else Abs(self.varname, term)
+        return out, reduced
+
+    def substitute(self, varname, term):
         """ Substitutes a particular variable by a term in this term. """
-        if self.bound_varname == name:
-            return self
+        # First calculate all free variables
         term = normalize_term(term)
-        if self.bound_varname in term.free_variables:
-            return self
-        return Abs(self.bound_varname, self.term.substitute(name, term))
+        if self.varname == varname:
+            # Do an alpha conversion of this expression but what name do we give this?
+            return self, True
+
+        this = self
+        if self.varname in term.free_variables:
+            fvt = term.free_variables
+            fve = self.term.free_variables
+            # naive way of finding an unbound var!
+            newname = ""
+            for i in xrange(1000000):
+                newname = "%s_%d" % (self.varname, i)
+                if newname not in fvt and newname not in fve:
+                    break
+            assert newname
+            subst,success = self.term.substitute(self.varname, newname)
+            this = Abs(newname, subst)
+        subst, success = this.term.substitute(varname, term)
+        out = this if not success else Abs(this.varname, subst)
+        return out, success
 
 class App(Term):
     """ Application of a source term to a target term. This does not yet result in an evaluation."""
@@ -115,30 +153,33 @@ class App(Term):
         Term.__init__(self)
         self.terms = map(normalize_term, terms)
 
-    def __repr__(self): return "App(%x): %s" % (id(self), str(self))
+    def __repr__(self): return "<App(%x): %s>" % (id(self), str(self))
     def __str__(self):
-        return "(" + " ".join(map(str, self.terms)) + ")"
+        return " ".join([str(t) if type(t) is Var else "(" + str(t) + ")" for t in self.terms])
 
-    @property
-    def free_variables(self):
+    def eval_free_variables(self):
         return reduce(lambda x,y: x.union(y), [t.free_variables for t in self.terms])
 
     def substitute(self, name, term):
         """ Substitutes a particular variable by a term in this term. """
-        term = normalize_term(term)
         substituted = [t.substitute(name, term) for t in self.terms]
-        return App(*substituted)
+        success = any(succeeded for result,succeeded in substituted)
+        return App(*(term for term,_ in substituted)), success
 
     def reduce_once(self):
-        out = self.terms[0]
-        if type(out) is not Abs: return self
-
         curr = 1
+        self.terms = [t.reduce()[0] for t in self.terms]
+        out = self.terms[0]
+        if type(out) is not Abs: return self, False
         while type(out) is Abs and curr < len(self.terms):
-            out = out.apply(self.terms[curr])
+            currterm = self.terms[curr]
+            result,_ = out.apply(currterm)
+            out = result
             curr += 1
-        return out if curr >= len(self.terms) else App(out, *self.terms[curr:])
+        out = out if curr >= len(self.terms) else App(out, *self.terms[curr:])
+        return out, True
 
+# Church Booleans
 true = Abs("t", Abs("f", "t"))
 false = Abs("t", Abs("f", "f"))
 test = Abs("l", Abs("m", Abs("n", App("l", "m", "n"))))
@@ -147,6 +188,23 @@ f_and = Abs("p", Abs("q", App("p", "q", "p")))
 f_or = Abs("p", Abs("q", App("p", "p", "q")))
 f_if = Abs("p", Abs("a", Abs("b", App("p", "a", "b"))))
 
+# Church Pairs
 pair = Abs("x", Abs("y", Abs("z", App("z", "x", "y"))))
 pair_first = Abs("p", App("p", Abs("x", Abs("y", "x"))))
 pair_second = Abs("p", App("p", Abs("x", Abs("y", "y"))))
+
+# Church numerals
+def church_lit(n):
+    base = "x"
+    while n > 0:
+        base = App("f", base)
+        n -= 1
+    return Abs(["f", "x"], base)
+zero = church_lit(0)
+
+plus = Abs(["m", "n", "f", "x"], App("m", "f", App("n", "f", "x")))
+succ = Abs(["n", "f", "x"], App("f", App("n", "f", "x")))
+pred = Abs(["n", "f", "x"], App("n", Abs(["g", "h"], App("h", App("g", "f"))), Abs("u", "x"), Abs("u", "u")))
+mult = Abs(["m", "n", "f"], App("m", App("n", "f")))
+minus = Abs(["m", "n"], App(App("n", pred), "m"))
+
